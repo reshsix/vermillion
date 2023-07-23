b57506961f73e12dd5190d8f9c1bd4aed08ade56
André Augusto <114283648+reshsix@users.noreply.github.com>
2023-07-23 17:56:14 -0300

    Added locale.h stub

---
+++ core/src/libc/locale.c
@@ -0,0 +1,60 @@
+/*
+This file is part of vermillion.
+
+Vermillion is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published
+by the Free Software Foundation, version 3.
+
+Vermillion is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with vermillion. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <_types.h>
+#include <limits.h>
+#include <locale.h>
+
+static const struct lconv locale =
+{
+    .decimal_point = ".",
+    .thousands_sep = "",
+    .grouping = "",
+    .int_curr_symbol = "",
+    .currency_symbol = "",
+    .mon_decimal_point = "",
+    .mon_thousands_sep = "",
+    .mon_grouping = "",
+    .positive_sign = "",
+    .negative_sign = "",
+    .int_frac_digits = CHAR_MAX,
+    .frac_digits = CHAR_MAX,
+    .p_cs_precedes = CHAR_MAX,
+    .n_cs_precedes = CHAR_MAX,
+    .p_sep_by_space = CHAR_MAX,
+    .n_sep_by_space = CHAR_MAX,
+    .p_sign_posn = CHAR_MAX,
+    .n_sign_posn = CHAR_MAX,
+    .int_p_cs_precedes = CHAR_MAX,
+    .int_n_cs_precedes = CHAR_MAX,
+    .int_p_sep_by_space = CHAR_MAX,
+    .int_n_sep_by_space = CHAR_MAX,
+    .int_p_sign_posn = CHAR_MAX,
+    .int_n_sign_posn = CHAR_MAX
+};
+
+extern char *
+setlocale(int category, const char *locale)
+{
+    (void)category, (void)locale;
+    return NULL;
+}
+
+extern struct lconv *
+localeconv(void)
+{
+    return (struct lconv *)&locale;
+}
