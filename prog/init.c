fae1872de86a26b6d865aa8ef14b68728de8fe20
André Augusto <114283648+reshsix@users.noreply.github.com>
2026-02-12 16:02:02 -0300

    Added program spawning to init shell

--- prog/init.c
+++ prog/init.c
@@ -3,6 +3,11 @@
 static char line[1024] = {0};
 static size_t line_c = 0;
 
+static char *list[256] = {0};
+static size_t list_c = 0;
+
+static char buffer[2048] = {0};
+
 extern bool
 vrm_entry(struct vrm *v, const char **args, int count)
 {
@@ -21,14 +26,59 @@ vrm_entry(struct vrm *v, const char **args, int count)
             v->comm.write0('\r');
             v->comm.write0('\n');
 
-            for (int i = 0; line[i] != '\0'; i++)
-                v->comm.write0(line[i]);
-            v->comm.write0('\r');
-            v->comm.write0('\n');
+            /* Parse program */
+            char *saveptr = NULL;
+            list[0] = v->str.token(line, " ", &saveptr);
+            if (list[0])
+                list_c++;
 
+            /* Parse args */
+            for (size_t i = 0; list[i] && i < 255; i++)
+            {
+                list[i + 1] = v->str.token(NULL, " ", &saveptr);
+                list_c++;
+            }
+
+            /* Make path */
+            v->mem.fill(buffer, '\0', sizeof(buffer));
+            v->str.copy(buffer, "/prog/", 6);
+            v->str.concat(buffer, list[0], v->str.length(list[0]));
+            v->str.concat(buffer, ".elf", 4);
+
+            /* Run program */
+            if (list_c)
+            {
+                uint32_t entry = 0;
+                uint8_t *mem = v->loader.prog(buffer, &entry);
+                if (mem)
+                {
+                    vrm_entry_t f = (void *)&(mem[entry]);
+                    v->syslog.string(f(v, list, list_c) ? "Success" :
+                                                          "Failure");
+                }
+                else
+                    v->syslog.string("ERROR: Program not found\r\n");
+                v->mem.del(mem);
+            }
+
+            /* Cleanup */
+            for (size_t i = 0; i < list_c; i++)
+                list[i] = NULL;
             line_c = 0;
+
+            /* Prompt */
             v->comm.write0('>');
             v->comm.write0(' ');
+        }
+        else if (c == '\b' || c == 0x7F)
+        {
+            if (line_c > 0)
+            {
+                v->comm.write0('\b');
+                v->comm.write0(' ');
+                v->comm.write0('\b');
+                line[--line_c] = '\0';
+            }
         }
         else
         {
